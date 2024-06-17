a305e40832475df4c4345666e3e004a2b1594bc7
bittradex <155362976+bittradex@users.noreply.github.com>
2024-06-17 17:24:14 +0100

    Update settings.py

--- vanguardfx/settings.py
+++ vanguardfx/settings.py
@@ -90,22 +90,22 @@ WSGI_APPLICATION = "vanguardfx.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'OIdyAkI8IKLWtr23',
-        'HOST': 'db.lshuwavlvcotrpkmqilg.supabase.co',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'OIdyAkI8IKLWtr23',
+#         'HOST': 'db.lshuwavlvcotrpkmqilg.supabase.co',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
