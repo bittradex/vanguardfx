40597bade5da49dd2e9582af97727373c20b3e93
bittradex <155362976+bittradex@users.noreply.github.com>
2024-06-17 20:04:05 +0100

    Update theme.js

--- static/js/theme.js
+++ static/js/theme.js
@@ -11,11 +11,11 @@
 	"use strict";
 
 // Preloader
-$(window).on('load', function () {
-	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
-	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(333);
-});
+// $(window).on('load', function () {
+// 	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
+// 	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
+// 	$('body').delay(333);
+// });
 
 
 // OTP Form (Focusing on next input)
